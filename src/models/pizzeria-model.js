68676bbaa15069f20b25608b700cb0f7ec3576cf
William(2052824) <2052824@cstj.qc.ca>
2022-11-22 09:27:56 -0500

    Fini Pizzeria model

--- src/models/pizzeria-model.js
+++ src/models/pizzeria-model.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
+import { PLANET_NAMES } from '../data/constants';
+import { MONSTER_ANCESTORS } from '../data/constants';
+import { PIZZA_TOPPINGS } from '../data/constants';
 
 const pizzeriaSchema = mongoose.Schema({
-
     planet: {
         required: true,
-        name: String,
+        enum: PLANET_NAMES,
     },
     coord: {
         lon: { type: Number, required: true, min: -1000, max: 1000 },
@@ -12,10 +14,11 @@ const pizzeriaSchema = mongoose.Schema({
     },
     chef: {
         name: { type: String, required: true },
-
+        ancestor: { enum: MONSTER_ANCESTORS, required: true },
+        speciality: { enum: PIZZA_TOPPINGS, required: true }
     }
 }, {
-    collection: 'explorations',
+    collection: 'pizzerias',
     id: false
 });
 

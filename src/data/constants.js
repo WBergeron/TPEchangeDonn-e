e87ccb09d10a1479d26dcfb2f2de7959eaba5fc7
Julius Leblanc <1974350@cstj.qc.ca>
2022-11-22 09:07:31 -0500

    Push des constant

---
+++ src/data/constants.js
@@ -0,0 +1,31 @@
+const MONSTER_ANCESTORS = [
+    'Sabi', 'Ouropi', 'Wyshiesk', 'Jklodriap', 'Vaznta', 'Vriofin', 'Vetaur',
+    'Thoren', 'Esinil', 'Scaisaro', 'Knoquix', 'Gorclun', 'Vapes', 'Bujkloo', 'Yrifsez', 'Kniobra', 'Thumera',
+    'Betinger', 'Vorelu', 'Graaha', 'Alecccue', 'Sessips', 'Haillest', 'Jine', 'Stabus', 'Fuaqon', 'Krouqn', 'Gnebeec',
+    'Uniffeips', 'Yiaearas', 'Peasine', 'Anuffeep', 'Jantump', 'Pusio', 'Galttio', 'Jiron', 'Ogy'
+];
+
+const PLANET_NAMES = [
+    'Neon 5', 'Phillon HZZK', 'Sigonides', 'Cuilara', 'Soivis', 'Thaepra', 'Mutrides', 'Chanruna', 'Edionope',
+    'Kucruigawa', 'Phiuq SIV', 'Zides BGG', 'Stretelara', 'Becipra', 'Nightandia', 'Ninia', 'Inzinda',
+    'Ucarvis', 'Gilveanides', 'Chelmaestea', 'Pualia', 'Porornania', 'Brion 8GW', 'Griuq 6ZML',
+    'Phapitis', 'Verizuno', 'Kaomia', 'Xatov', 'Hondarvis', 'Pulreshan', 'Goceohiri', 'Postraigantu',
+];
+
+const PIZZA_SIZES = ['Small', 'Medium', 'Large', 'X-Large'];
+
+const FRUITS = [
+    'Lemon', 'Pineapple', 'Strawberry', 'Apple', 'Avocado', 'Blackberries', 'Blueberries', 'Coconut',
+    'Cherries', 'Grapes', 'Kiwi', 'Lime', 'Mango', 'Orange', 'Pear', 'Watermelon', 'Raspberries'
+];
+const VEGETABLES = [
+    'Zucchini', 'Hot Banana Pepper', 'Bruschetta', 'Green Peppers', 'Mushrooms', 'Jalapeño',
+    'Black Olives', 'Spinach', 'Red Onions', 'Red Peppers', 'Artichokes', 'Brocoli', 'Green Olive', 'Garlic'
+];
+const MEATS = [
+    'Pepperoni', 'Sausage', 'Chicken', 'Bacon', 'Ham', 'Chorizo', 'Salami', 'Anchovies', 'Beef',
+    'Salami', 'Spicy Italian Sausage', 'Egg', 'Peanut Butter', 'Octopus', 'Tuna'];
+
+const PIZZA_TOPPINGS = [...FRUITS, ...VEGETABLES, ...MEATS];
+
+export { PLANET_NAMES, MONSTER_ANCESTORS, PIZZA_SIZES, PIZZA_TOPPINGS };
